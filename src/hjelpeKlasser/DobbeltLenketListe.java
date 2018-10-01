3839bcfe72677b22c087cecdde7cc67ffb30b235
s929577 <s929577@oslomet.no>
2018-10-01 22:52:25 +0200

    Working with Oppgave 2 stringbuild

--- src/hjelpeKlasser/DobbeltLenketListe.java
+++ src/hjelpeKlasser/DobbeltLenketListe.java
@@ -3,6 +3,7 @@ package hjelpeKlasser;
 import hjelpeKlasser.Liste;
 
 import java.util.Iterator;
+import java.util.Objects;
 
 public class DobbeltLenketListe<T> implements Liste<T>{
 
@@ -16,23 +17,117 @@ public class DobbeltLenketListe<T> implements Liste<T>{
             this.nestepeker = nestepeker;
             this.forrigepeker = forrigepeker;
         }
+
+        private Node(T verdi){
+            this(verdi,null, null);
+        }
+
     }
 
 
  //instansvariables
     private Node<T> hode, hale;
     private int antall;
-    private int antallEndringer;
+    private int endringer;
 
 
-    public DobbeltLenketListe(){
+    public DobbeltLenketListe(){  //standardKonstrucktor
 
         hode = hale = null;
         antall = 0;
-        antallEndringer = 0;
+        endringer = 0;
+    }
+
+    /**
+     * konstruktør
+     * Oppgave 1b
+     * @param a
+     */
+    public DobbeltLenketListe(T[] a){
+        this();   // alle variabelene er nullet ved bruke av stardardKonstrucktor
+
+        Objects.requireNonNull(a, "Null verdi er Ulovlig");
+        hode = hale = new Node<>(null);
+
+        for(T verdi : a){
+            if(verdi != null){
+                hode = hale = new Node<>(verdi, hale, null);
+                antall++;
+            }
+        }
+        if(a.length == 0){
+            hode = hale = null;// hvis tomt skall de være null
+        }else{
+            hode = hode.nestepeker.forrigepeker = null; //Spesielt skal både hode.forrigeoghale.nestevære null.
+        }
     }
 
 
+    /**
+     * Oppgave 2
+     * @return
+     */
+    @Override
+    public String toString(){
+        if(tom()) return "[]";
+        StringBuilder txt = new StringBuilder();
+
+        Node<T> pointer = hode;
+        txt.append(pointer.verdi);
+        pointer = pointer.nestepeker;
+
+        while (pointer != null){
+            txt.append(",").append("").append(pointer.verdi);
+            pointer = pointer.nestepeker;
+        }
+        txt.append("]");
+        return txt.toString();
+    }
+//    public String toString(){
+//        StringBuilder txt = new StringBuilder();
+//
+//        txt.append("[");
+//        Node<T> pointer = hode;
+//        if(pointer != null) txt.append(pointer.verdi);
+//       while (pointer != null && pointer.nestepeker != null){
+//           txt.append(" ," + pointer.nestepeker.verdi);
+//           pointer = pointer.nestepeker;
+//       }
+//       txt.append("]");
+//       return txt.toString();
+//
+//    }
+
+    /**
+     *
+     * @return
+     */
+    public String omvendtString() {
+
+        if(tom()) return "[]";
+        StringBuilder txt = new StringBuilder();
+
+        Node<T> pointer = hale;
+        txt.append(pointer.verdi);
+        pointer = pointer.forrigepeker;
+
+        while (pointer != null){
+            txt.append(",").append("").append(pointer.verdi);
+            pointer = pointer.forrigepeker;
+        }
+        txt.append("]");
+        return txt.toString();
+
+//        txt.append("[");
+//        Node<T> pointer = hale;
+//        if(pointer != null) txt.append(pointer.verdi);
+//        while (pointer != null && pointer.forrigepeker != null){
+//            txt.append(" ," + pointer.forrigepeker.verdi);
+//            pointer = pointer.forrigepeker;
+//        }
+//        txt.append("]");
+//        return txt.toString();
+    }
 
     @Override
     public boolean leggInn(T verdi) {
@@ -74,11 +169,19 @@ public class DobbeltLenketListe<T> implements Liste<T>{
         return null;
     }
 
+    /**
+     * Oppgave 1
+     * @return
+     */
     @Override
     public int antall() {
         return antall;
     }
 
+    /**
+     * Oppgave 1
+     * @return
+     */
     @Override
     public boolean tom() {
         return antall == 0;
@@ -93,4 +196,47 @@ public class DobbeltLenketListe<T> implements Liste<T>{
     public Iterator<T> iterator() {
         return null;
     }
+
+    public Iterator<T> iterator(int indeks)
+    {
+        throw new UnsupportedOperationException("Ikke laget ennå!");
+    }
+
+    private class DobbeltLenketListeIterator implements Iterator<T>
+    {
+        private Node<T> denne;
+        private boolean fjernOK;
+        private int iteratorendringer;
+
+        private DobbeltLenketListeIterator()
+        {
+            denne = hode;     // denne starter på den første i listen
+            fjernOK = false;  // blir sann når next() kalles
+            iteratorendringer = endringer;  // teller endringer
+        }
+
+        private DobbeltLenketListeIterator(int indeks)
+        {
+            throw new UnsupportedOperationException("Ikke laget ennå!");
+        }
+
+        @Override
+        public boolean hasNext()
+        {
+            return denne != null;  // denne koden skal ikke endres!
+        }
+
+        @Override
+        public T next()
+        {
+            throw new UnsupportedOperationException("Ikke laget ennå!");
+        }
+
+        @Override
+        public void remove()
+        {
+            throw new UnsupportedOperationException("Ikke laget ennå!");
+        }
+
+    } // DobbeltLenketListeIterator
 }
--- src/hjelpeKlasser/Main.java
+++ src/hjelpeKlasser/Main.java
@@ -5,6 +5,31 @@ public class Main {
     public static void main(String[] args) {
         Liste<String> liste = new DobbeltLenketListe<String>();
 
+        loopit("Oppgave1");
+        System.out.println();
+
         System.out.println(liste.antall() + " " + liste.tom());
+
+
+        String[] s = {"Ole", null, "Per", null, null, null};
+        Liste<String> listeNavn = new DobbeltLenketListe<>(s);
+
+        loopit("Oppgave2");
+
+        String[] s1 = {}, s2 = {"A"}, s3 = {null,"A",null,"B",null};
+        DobbeltLenketListe<String> l1 = new DobbeltLenketListe<>(s1);
+        DobbeltLenketListe<String> l2 = new DobbeltLenketListe<>(s2);
+        DobbeltLenketListe<String> l3 = new DobbeltLenketListe<>(s3);
+        System.out.println();
+        System.out.println(l1.toString() + " "+ l2.toString()
+                + " "+ l3.toString() + " "+ l1.omvendtString()
+                + " "+ l2.omvendtString() + " "+ l3.omvendtString());
+    }
+
+    public static void loopit(String label){
+        System.out.println(label);
+        for (int i = 0; i < label.length(); i++) {
+            System.out.print("-");
+        }
     }
 }
